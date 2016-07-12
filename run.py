87f3e8a008f72391af7bf0bb977884b001cfec5e
Yinzo <oop995@gmail.com>
2016-07-12 19:15:25 +0800

    PostMail first commit

---
+++ run.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from postmail import MailSender
+
+app = Flask(__name__)
+mail_sender = MailSender()
+
+
+@app.route('/')
+def homepage():
+    return "MailSender is online!"
+
+@app.route('/mail', methods=['POST'])
+def mail():
+    mail_sender.send(**request.form.to_dict())
+    try:
+        return jsonify({'status': 'succeed', 'msg': ""})
+    except Exception as ex:
+        return jsonify({'status': 'failed', 'msg': str(ex)})
+
+if __name__ == '__main__':
+    app.run()
