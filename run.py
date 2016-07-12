76b3698570e73b3b348d56300146fcfff0fc37e0
Yinzo <oop995@gmail.com>
2016-07-12 23:14:48 +0800

    Fix: put the sending code back into try block

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ def homepage():
 
 @app.route('/mail', methods=['POST'])
 def mail():
-    mail_sender.send(**request.form.to_dict())
     try:
+        mail_sender.send(**request.form.to_dict())
         return jsonify({'status': 'succeed', 'msg': ""})
     except Exception as ex:
         return jsonify({'status': 'failed', 'msg': str(ex)})
